b1caa697bf7f77adc812e8bbca6501fc38509398
zenoWS <zeno9302@naver.com>
2024-10-24 02:11:41 +0900

    [FIX] 패키지 주소 변경

--- src/main/java/com/wearei/finalsamplecode/FinalSampleCodeApplication.java
+++ src/main/java/com/wearei/finalsamplecode/FinalSampleCodeApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.wearei.finalsamplecode;
+package com.wearei.finalsamplecode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
