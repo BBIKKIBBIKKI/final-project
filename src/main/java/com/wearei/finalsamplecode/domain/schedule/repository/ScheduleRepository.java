c52de5ab215cd4c3d57fc085e359c7453f61284a
zenoWS <zeno9302@naver.com>
2024-10-24 02:14:42 +0900

    [FEAT] 일정레포지토리에 구단아이디로 일정조회하는 로직 추가

--- src/main/java/com/wearei/finalsamplecode/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/wearei/finalsamplecode/domain/schedule/repository/ScheduleRepository.java
@@ -2,6 +2,8 @@ package com.wearei.finalsamplecode.domain.schedule.repository;
 
 import com.wearei.finalsamplecode.domain.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findByTeamId(Long teamId);
 }
