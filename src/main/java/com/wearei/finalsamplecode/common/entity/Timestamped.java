7c3e8430ee83086048d271345bebc977ff15ac3f
Eunkyo <jeongeunkyo3@gmail.com>
2024-10-25 03:11:16 +0900

    [FIX] Timestamped 복구

--- src/main/java/com/wearei/finalsamplecode/common/entity/Timestamped.java
+++ src/main/java/com/wearei/finalsamplecode/common/entity/Timestamped.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class Timestamped extends BaseEntity{
+public abstract class Timestamped{
 
     @CreatedDate
     @Column(updatable = false)
