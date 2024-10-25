6cc4562e788b673c45aa7b0d3cbda29a4a9d007b
HyunKook <gusrnr5153@gmail.com>
2024-10-25 11:26:52 +0900

    test 하는 중

---
+++ src/test/java/com/wearei/finalsamplecode/domain/menu/service/MenuServiceTest.java
@@ -0,0 +1,28 @@
+package com.wearei.finalsamplecode.domain.menu.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuServiceTest {
+
+    @Test
+    void createMenu() {
+    }
+
+    @Test
+    void updateMenu() {
+    }
+
+    @Test
+    void deleteMenu() {
+    }
+
+    @Test
+    void checkMenu() {
+    }
+
+    @Test
+    void findAllMenus() {
+    }
+}
---
+++ src/test/java/com/wearei/finalsamplecode/domain/store/service/StoreServiceTest.java
@@ -0,0 +1,44 @@
+package com.wearei.finalsamplecode.domain.store.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreServiceTest {
+
+    @Test
+    void createStore() {
+    }
+
+    @Test
+    void updateStore() {
+    }
+
+    @Test
+    void getAllStores() {
+    }
+
+    @Test
+    void getStore() {
+    }
+
+    @Test
+    void searchStoresOrMenus() {
+    }
+
+    @Test
+    void deleteStore() {
+    }
+
+    @Test
+    void checkUser() {
+    }
+
+    @Test
+    void checkStore() {
+    }
+
+    @Test
+    void authCheck() {
+    }
+}
