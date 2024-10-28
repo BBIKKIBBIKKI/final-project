28eec285c0e08a6576b06d1359ac653877f1b38f
Eunkyo <jeongeunkyo3@gmail.com>
2024-10-28 21:07:31 +0900

    [FIX] 엔터 삭제

--- src/main/java/com/wearei/finalsamplecode/domain/team/service/TeamService.java
+++ src/main/java/com/wearei/finalsamplecode/domain/team/service/TeamService.java
@@ -16,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 
 @Service
